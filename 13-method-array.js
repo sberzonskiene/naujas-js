43abc2878d2b5d1be75b64d254275e04d49be271
sberzonskiene <s.berzonskiene@gmail.com>
2025-04-07 09:41:40 +0300

    update

--- 13-method-array.js
+++ 13-method-array.js
@@ -65,3 +65,70 @@ for (let i = 0; i < n1.length; i++) {
     n12.push(n2[i]);
 }
 console.log(n12);
+
+const numbers2 = [ 10, 2, 8, 4, 6];
+console.log(numbers2);
+
+numbers2.push(9);
+console.log(numbers2);
+
+numbers2.pop(); // pasalina is galo
+numbers2.pop();
+console.log(numbers2);
+
+const g1 = numbers2.pop(); // ne tik pasalina, bet ir grazina ka pasalino.
+const g2 = numbers2.pop();
+console.log(numbers2, g1, g2);
+
+numbers2.unshift(1); // padeda i prieki
+numbers2.unshift(2);
+numbers2.unshift(3);
+console.log(numbers2);
+
+numbers2.shift();      // pasalina pirma elementa ir ji gali grazinti
+console.log(numbers2);
+
+const g3 = numbers2.shift(); 
+const g4 = numbers2.shift ();
+console.log(numbers2, g3, g4);
+
+console.log('-----------');
+
+//0              0   1   2   3   4
+const magic = [ 11, 22, 33, 44, 55];
+console.log(magic.includes(5));
+console.log(magic.includes(55));
+
+console.log(magic.indexOf(5));
+console.log(magic.indexOf(55));
+
+const text = [ 'morka', 'bulve', 'kopustas', 'cesnakas'];
+
+//Rekalingi produktai: a, b, c, d.
+const products = `Reikalingi produktai: ${ text.join (', ')}.`; 
+console.log(products);
+
+console.log('-----------');
+
+const texts = [ 'morka', 'bulve', 'kopustas', 'cesnakas'];
+console.log(texts);
+texts.reverse();
+console.log(texts);
+
+const c1 = [1, 11];
+const c2 = [2, 22];
+const c3 = [3, 33];
+const c4 = [4, 44];
+
+const c12 = c1.concat(c2);
+console.log(c12);
+
+const c34 = c3.concat(c4);
+console.log(c34);
+
+const c123 = c1.concat(c2).concat(c3);
+console.log(c123);
+
+const c111 = c1.concat(c1, c1);
+console.log(c111);
+
---
+++ task/names-split-join.js
@@ -0,0 +1,41 @@
+/*
+Funkcija gauna sakini, kuris atrodo taip:
+"Siuo metu klaseje yra: jonas, Maryte, peTRas. ONA."
+Funkcijos darbas- grazinti teisingai perrasyta sakini.
+T.y. reikia istaisyti vardu rasyma.
+pvz.: "Siuo metu klaseje yra: Jonas, Maryte, Petras, Ona."
+*/
+
+/*
+- atsiskiriame vardus is orginalaus teksto
+- konvertuojama teksta i mazasias raides
+- issiskaidyti individualius vardus i masyva
+- ciklas eina per individualius vardus ir:
+    - pasiima pirma raide
+    - ja padidina
+    - apjungia i galutini normalu varda
+- pagaminame galutini reikiama sakini 
+*/
+
+function correctSentence(text) {
+    const parts = text.split(': ');
+    const names = parts[1]
+    .slice(0, -1)
+    .toLowerCase()
+    .split(', ');
+
+    for (let i = 0; i < names.length; i++) {
+        const name = names[i];
+        const firstLetter = name[0].toUpperCase();
+        const restName = name.slice(1);
+
+        const newName = firstLetter + restName;
+        names[i] = newName;
+
+    }
+
+    console.log(names);
+
+    return parts[0] + ': ' + names.join(', ')+ '.';
+}
+ console.log(correctSentence('Siuo metu klaseje yra: jonas, Maryte, peTRas, ONA.'));
