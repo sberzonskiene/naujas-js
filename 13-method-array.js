ef434a86cd0bb17294684cc09503b57b2144d838
sberzonskiene <s.berzonskiene@gmail.com>
2025-04-08 11:21:57 +0300

    update

--- 13-method-array.js
+++ 13-method-array.js
@@ -146,3 +146,92 @@ console.log(h);
 
 h.splice(1, 2);
 console.log(h);
+
+console.clear();
+
+// MAP
+const k = [10, 2, 8,4, 6];
+const k2 = [];
+
+for (const n of k) {
+    k2.push(n * 2);
+}
+console.log(k2);
+
+function triple(n) {
+    return n * 3;
+}
+
+const k3 = k.map(triple);
+console.log(k3);
+
+const quadro = n => n * 4;
+const k4 = k.map(quadro);
+console.log(k4);
+
+const k5 = k.map(n => n * 5);
+console.log(k5);
+
+const k6 = k.map(n => n);
+console.log(k6);
+
+const k7 = k.map(n => n + 1);
+console.log(k7);
+
+// MAP su tekstais - object
+const dict = ['bulve', 'morka', 'svogunas'];
+const dict2 = dict.map(w => w[0])
+console.log(dict2);
+
+const dict3 = dict.map(w => w.length)
+console.log(dict3);
+
+
+const people = [
+    {name: 'Jonas', age: 99 },
+    {name: 'Maryte', age: 88},
+    {name: 'Petras', age: 77},
+    {name: 'Ona', age: 55},
+];
+
+const people100 = people.map(person =>`${person.name} liko ${100 - person.age} metai.`);
+console.log(people100);
+
+const peopleNames = people.map(person => person.name);
+console.log(peopleNames);
+
+const peopleAges = people.map(person => person.age);
+console.log(peopleAges);
+
+const peopleMarried = people.map(person => {
+    person.isMarried = true;
+    return person;
+});
+console.log(peopleMarried);
+
+// FILTER
+const p = [10, 2, 8, 4, 6];
+const p5 = [];
+
+for (const n of p) {
+    if (n > 5) {
+        p5.push(n);
+    }
+}
+console.log(p5);
+
+const p6 = p.filter(n => n > 6);
+console.log(p6);
+
+const p7 = p.filter(n => n <= 7);
+console.log(p7);
+
+const p8 = p.filter(n => n > 2 && n < 8);
+console.log(p8);
+
+const texts5 = ['labas', '', 'rytas', '', 'Lietuva'];
+const texts5Updated = texts5.filter(t => t.length > 0);
+console.log(texts5Updated);
+
+const texts5Updated2 = texts5.filter(t => t); 
+console.log(texts5Updated2);
---
+++ 18-for-in.js
@@ -0,0 +1,70 @@
+const person ={
+    name: 'Jonas',
+    age: 99,
+    isMarried: true,
+};
+
+const keys = Object.keys(person);
+console.log(keys);
+
+for (const key of keys) {
+    console.log(person[key]);
+}
+
+// for-in dirba tik su objektais
+for (const key in person) {
+    console.log(person[key]);
+}
+
+// 1)
+const text = 'Labas';
+// 2)
+const counts = {
+    'L': 1,
+    'a': 2,
+    'b': 1,
+    's': 1,
+};
+// 3)
+const max = 2;
+const letter = 'a'
+console.log('---')
+
+//1)
+const text2 = 'Labas';
+const symbols = {};
+
+symbols['L'] = 1;
+symbols['a'] = 1;
+symbols['b'] = 1;
+symbols['a'] += 1;
+symbols['s'] = 1;
+console.log(symbols);
+
+const text3 = 'pavasaris';
+const symbols3 = {};
+
+for (const s of text3) {
+    if (symbols3[s]){
+        symbols3[s]++;
+    }    else {
+            symbols3[s] = 1
+        }
+    }
+console.log(symbols3);
+
+let max1 = 0;
+for (const s in symbols3) {
+    if (symbols3[s] > max1) {
+        max1 = symbols3[s];
+    }
+}
+console.log(max1);
+
+const maxSymbols3 = [];
+for ( const s in symbols3) {
+    if (symbols3[s] === max1)
+        maxSymbols3.push(s);
+}
+console.log(maxSymbols3);
+
