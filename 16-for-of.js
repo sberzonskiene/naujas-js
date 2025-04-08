714274901766ba543310a60df28e01c6b91bf2ef
sberzonskiene <s.berzonskiene@gmail.com>
2025-04-08 09:49:00 +0300

    update

---
+++ 16-for-of.js
@@ -0,0 +1,32 @@
+const marks = [10, 2, 8, 4, 6];
+let sum = 0;
+
+for (let i = 0; i < marks.length; i++) {
+    const mark = marks[i];
+    sum += mark;
+}
+
+console.log(sum);
+
+let sum2 = 0;
+
+for (const mark of marks) {
+    sum2 += mark;
+}
+console.log(sum2);
+
+const names = [ 'Jonas', 'Maryte', 'Petras', 'Ona'];
+
+for (const name of names) {
+    console.log(name);
+}
+
+const names1 = ['Tiesa', 'Sviesa', 'Geris'];
+const namesSizes = [];
+for (const name1 of names1) 
+    {
+    namesSizes.push(name1.length);
+}
+console.log(namesSizes);
+
+
